6dcba8efd61c3a60b31a94e668e0903d001ef5c9
esellier <emiliesellier21@gmail.com>
2025-10-25 20:49:43 +0200

    just need to clean

--- sources/dockers/mariadb/tools/script.sh
+++ sources/dockers/mariadb/tools/script.sh
@@ -2,45 +2,47 @@
 # --> shebang = script need to be execute with bash 
 
 # --> start temporarily the server to init SQL commands
-#initialize_service()
-
-#	service mariadb start
-#	sleep 1
-#}
+initialize_service()
+{
+	service mariadb start
+	sleep 1
+	mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+}
 
 # --> apply good securities practices
+# --> mariadb -e = program + execute, execute this command
 initialize_securities()
 {
-	# --> remove anonymous users
-	mariadb -e "DELETE FROM mysql.user WHERE User='';"
+    # --> remove anonymous users
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "DELETE FROM mysql.user WHERE User='';"
 
-	# --> reject remote root login
-	mariadb -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    # --> reject remote root login
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 
-	# --> remove test database & his privileges
-	mariadb -e "DROP DATABASE IF EXISTS test;"
-	mariadb -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
+    # --> remove test database & his privileges
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS test;"
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 
-	# --> reload privilege tables
-	mariadb -e "FLUSH PRIVILEGES;"
+    # --> reload privilege tables
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 }
-# --> mariadb -e = program + execute, execute this command
 
 # --> create base & user with env
 initialize_database()
 {
-	mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-	mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
-	#mariadb -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER_NAME'@'%';"
-	mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER_NAME'@'%' WITH GRANT OPTION;"
-	mariadb -e "FLUSH PRIVILEGES;"
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${DB_USER_NAME}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER_NAME}'@'%' WITH GRANT OPTION;"
+    mariadb -u root -p"${DB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    
+    #give access to the database file to the user
+    chown -R mysql:mysql /var/lib/mysql/${DB_NAME}
 }
 
-# --> stop the configuration server
-#initialize_service
+initialize_service
 initialize_securities
 initialize_database
-#service mariadb stop
+service mariadb stop
 
 # --> execute final CMD (from Dockerfile)
 exec "$@"
--- sources/dockers/wordpress/tools/script.sh
+++ sources/dockers/wordpress/tools/script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+#--> change repertory
 cd /var/www/html
+
+#--> download WP-CLI program (install & manage WP without graphic interface)
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
+
+#--> download Wordpress
 ./wp-cli.phar core download --allow-root
+
+#--> create WP configuration
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER_NAME --dbpass=$DB_USER_PASSWORD --dbhost=mariadb --allow-root
+
+#--> install WP
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=$WP_USER_NAME --admin_password=$WP_USER_PASSWORD --admin_email=$WP_ADMIN_MAIL --allow-root
 
-#exec "$@"
+#--> update URL in database
+./wp-cli.phar option update siteurl "https://${DOMAIN_NAME}" --allow-root
+./wp-cli.phar option update home "https://${DOMAIN_NAME}" --allow-root
 
+#--> throw PHP-FPM to execute WP
 php-fpm7.4 -F
 
-#WP-CLI program to install & manage WP without graphic interface
-#WordPress is downloaded
-#connexion to dabase configurated
-#wordpress installed with a user
-#PHP server launch to execute the website
